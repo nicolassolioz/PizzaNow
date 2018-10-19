1f48da08ca13c25c10954f8a5651173667663c71
Nicolas Solioz <nicolas.solioz@students.hevs.ch>
2018-10-19 09:53:34 +0200

    Initial commit

---
+++ app/src/main/java/com/mycompany/pizzanow/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mycompany.pizzanow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //SALUT PIZZA NOW CE GIT VA MARCHER
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
