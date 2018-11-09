3c0acbdfc53be302671be0395284355f5dc7cc7b
Bornatch <valentin.bornatici@gmail.com>
2018-11-09 08:25:16 +0100

    Suite db

--- app/src/main/java/com/mycompany/pizzanow/entity/PosEntity.java
+++ app/src/main/java/com/mycompany/pizzanow/entity/PosEntity.java
@@ -4,65 +4,142 @@ package com.mycompany.pizzanow.entity;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.support.annotation.NonNull;
+import android.view.Menu;
 
 import com.mycompany.pizzanow.model.Pos;
+
+import java.util.Locale;
 
 @Entity(tableName = "filiale", primaryKeys = {"idFiliale"})
 public class PosEntity implements Pos, Comparable{
 
     @NonNull
-    private int idFiliale;
+    private int IdFiliale;
 
     @ColumnInfo(name = "Nom")
-    private String nom;
+    private String Nom;
 
+    @ColumnInfo(name = "Adresse")
+    private String Adress;
+
+    @ColumnInfo(name = "NPA")
+    private int Npa;
+
+    @ColumnInfo(name = "Localite")
+    private String Localite;
+
+    @ColumnInfo(name = "Responsable")
+    private int Responsable;
+
+    @ColumnInfo(name = "Email")
+    private String Email;
+
+    @ColumnInfo(name = "Phone")
+    private String Phone;
+
+    @ColumnInfo(name = "Menu")
+    private int IdMenu;
+
+    public PosEntity(){}
+
+    public PosEntity(Pos pos){
+        IdFiliale=getidFiliale();
+        Nom=getNom();
+        Adress=getAdresse();
+        Npa=getNPA();
+        Localite=getLocalite();
+        Responsable=getResponsable();
+        Email=getEmail();
+        Phone=getPhone();
+        IdMenu=getMenu();
+    }
+
+    public PosEntity(@NonNull int idFiliale, String nom, String adress, int npa,
+                     String localite, int responsable, String email, String phone,
+                     int idMenu){
+        this.IdFiliale=idFiliale;
+        this.Nom=nom;
+        this.Adress=adress;
+        this.Npa=npa;
+        this.Localite=localite;
+        this.Responsable=responsable;
+        this.Email=email;
+        this.Phone=phone;
+        this.IdMenu=idMenu;
+    }
+
+    @NonNull
     @Override
     public int getidFiliale() {
-        return 0;
+        return IdFiliale;
     }
+
+    public void setIdFiliale(@NonNull int idFiliale) {this.IdFiliale = idFiliale;}
 
     @Override
     public String getNom() {
-        return null;
+        return Nom;
     }
 
-    @Override
-    public String getAdresse() {
-        return null;
-    }
+    public void setNom(String nom){this.Nom = nom;}
 
     @Override
-    public int getNPA() {
-        return 0;
-    }
+    public String getAdresse() { return Adress; }
+
+    public void setName(String nom){this.Nom=nom;}
+
+    @Override
+    public int getNPA() { return Npa; }
+
+    public void setNpa(int npa) {this.Npa=npa;}
 
     @Override
     public String getLocalite() {
-        return null;
+        return Localite;
     }
+
+    public void setLocalite(String localite){this.Localite=localite;}
 
     @Override
     public int getResponsable() {
-        return 0;
+        return Responsable;
     }
+
+    public void setResponsable(int responsable){ this.Responsable=responsable;}
 
     @Override
     public String getEmail() {
-        return null;
+        return Email;
     }
+
+    public void setEmail(String email) {this.Email=email;}
 
     @Override
     public String getPhone() {
-        return null;
+        return Phone;
+    }
+
+    public void setPhone(String phone){this.Phone=phone;}
+
+    @Override
+    public int getMenu() { return IdMenu; }
+
+    public void setIdMenu(int idMenu){ this.IdMenu=idMenu;}
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null) return false;
+        if(obj == this) return true;
+        if(!(obj instanceof PosEntity)) return false;
+        PosEntity o = (PosEntity) obj;
+        return o.getidFiliale() == this.getidFiliale();
     }
 
     @Override
-    public int getMenu() {
-        return 0;
-    }
+    public String toString(){return Nom + " " + Localite;}
 
     @Override
     public int compareTo(@NonNull Object o) {
-        return 0;
+        return toString().compareTo(o.toString());
     }
 }
