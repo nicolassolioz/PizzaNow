bddea2fea01df1d777a248a5a0b51a489d7d1bf3
Bornatch <valentin.bornatici@gmail.com>
2018-11-14 01:18:04 +0100

    Affichage des POS dasn la MainActivity, transfert de l'ID daans la FragmentActivity et récupération dans les fragments

--- app/src/main/java/com/mycompany/pizzanow/ui/MainActivity.java
+++ app/src/main/java/com/mycompany/pizzanow/ui/MainActivity.java
@@ -6,7 +6,10 @@ import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 import com.mycompany.pizzanow.R;
@@ -17,6 +20,8 @@ import com.mycompany.pizzanow.database.entity.CollaborateurEntity;
 import com.mycompany.pizzanow.database.entity.PizzaEntity;
 import com.mycompany.pizzanow.database.entity.PosEntity;
 import com.mycompany.pizzanow.model.Pizza;
+import com.mycompany.pizzanow.util.RecyclerViewItemClickListener;
+import com.mycompany.pizzanow.viewmodel.POS.PosListViewModel;
 import com.mycompany.pizzanow.viewmodel.pizza.CollaborateurViewModel;
 import com.mycompany.pizzanow.viewmodel.pizza.PizzaViewModel;
 
@@ -24,6 +29,7 @@ import android.arch.persistence.room.*;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observer;
 
@@ -31,14 +37,15 @@ public class MainActivity extends ToolbarActivity {
 
     private AppDatabase db;
 
+    private static final String TAG = "MainActivity";
+
     private CollaborateurViewModel mViewModel;
     private CollaborateurEntity mCollaborateur;
     private TextView mEtPizzaName;
 
     private List<PosEntity> mPosEntities;
     private RecyclerAdapter<PosEntity> mAdapter;
-    //private TextView mEtPizzaDescription;
-    //private TextView mEtPizzaPrice;
+    private PosListViewModel mListViewmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,47 @@ public class MainActivity extends ToolbarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEtPizzaName  = (TextView) findViewById(R.id.textTest);
+
+        /*affichage des succursales : init*/
+        RecyclerView recyclerView = findViewById(R.id.CityRecyclerView);
+        //layout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //separateurs
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        //définition de la liste et des comportements
+        mPosEntities = new ArrayList<>();
+        mAdapter = new RecyclerAdapter<>(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent intent = new Intent(MainActivity.this, ShowSuccActivity.class);
+                intent.putExtra("posID", mPosEntities.get(position).getIdFiliale());
+                startActivity(intent);
+            }
+
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+                Log.d(TAG, "longClicked position:" + position);
+                Log.d(TAG, "longClicked on: " + mPosEntities.get(position).getNom());
+
+            }
+        });
+        // ouverture du factory
+        PosListViewModel.Factory factoryPos = new PosListViewModel.Factory(getApplication());
+        mListViewmodel = ViewModelProviders.of(this, factoryPos).get(PosListViewModel.class);
+        mListViewmodel.getAllPos().observe(this,posEntities -> {
+            if(posEntities!=null){
+                mPosEntities = posEntities;
+                mAdapter.setData(mPosEntities);
+            }
+        });
+
+        recyclerView.setAdapter(mAdapter);
+
+        /*affichage des succursales : fin*/
 
         String nomCollaborateur = "Bocelli";
 
